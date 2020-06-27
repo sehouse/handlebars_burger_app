de608a010b56265fda42047fc6c5e8c8549ba4ab
sehouse <sehouse@umich.edu>
2020-06-27 17:36:05 -0400

    burger.js is written.

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,24 @@
 //import orm.js into burger.js
+const orm = require('../config/orm.js');
 //create the code to call the orm functions using burger specific input for the orm
+
+const burger = {
+    all: (cb) => {
+        orm.all('burgers', (result) => {
+            cb(result)
+        });
+    },
+    create: (cols, vals, cb) => {
+        orm.create('burgers', cols, vals, (result) => {
+            cb(result);
+        });
+    },
+    update: (objColVals, condition, cb) => {
+        orm.update('burgers', objColVals, condition, (result) => {
+            cb(result);
+        });
+    }
+};
+
 //export at the end
+module.exports = burger;

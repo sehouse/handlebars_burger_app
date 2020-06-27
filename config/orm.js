dc3b145a784cf0c5f682b22c4a49c22b4106af04
sehouse <sehouse@umich.edu>
2020-06-27 17:29:45 -0400

    ORM is written.

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,80 @@
 //import connection.js into orm.js
-//create mysql commands:
+const connection = require('../config/connection.js');
+
+//Helper functions for SQL syntax
+const printQuestionMarks = (number)=>{
+    let array = [];
+    for (let i = 0; i < number; i++){
+        array.push('?');
+    }
+    return array.toString();
+}
+
+const objToSql = (object) => {
+    let array = [];
+    for (let key in object) {
+        if (Object.hasOwnProperty.call(object, key)) {
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'";
+            }
+            array.push(key + '=' + value);
+        }
+    }
+    return array.toString();
+}
+
+//ORM SQL functions
+const orm = {
     //selectAll
+    all: (tableInput, cb) => {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
     //insertOne
-    //updateOne
+    create:  (table, cols, vals, cb) => {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+  //updateOne
+    update: (table, objColVals, condition, cb) => {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    }
+};
+
 //export orm in module.exports
+module.exports = orm;

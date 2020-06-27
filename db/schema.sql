318def8e090c5ee23697230ad9336928e4567781
sehouse <sehouse@umich.edu>
2020-06-27 16:33:27 -0400

    DB schema created.

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,29 @@
 -- create burgers_db
+CREATE DATABASE burgers_db;
+
 --switch to burgers_db
---create burgers tabe with fields:
-    --id: aut incrementing int that serves as primary key
+USE burgers_db;
+
+--create burgers table with fields:
+
+CREATE TABLE burgers(
+
+    --id: aut incrementing int
+
+    id int NOT NULL AUTO_INCREMENT,
+
     --burger_name: string
+
+    burger_name VARCHAR(100) NOT NULL,
+
     --devoured: boolean
+
+    devoured BOOLEAN NOT NULL,
+
+    --id serves as primary key
+    PRIMARY KEY(id)
+);
+
+
+    
     

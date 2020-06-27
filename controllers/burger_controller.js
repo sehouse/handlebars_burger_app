20247dca6c215ca3709bac49adfac7f736deaafd
sehouse <sehouse@umich.edu>
2020-06-27 18:14:52 -0400

    burger_controller.js is written.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,3 +1,30 @@
-//import express
-//import burger.js
-//create router for the app and export the router at the end of the file
+const express = require('express');
+const router = express.router();
+const burger = require('../models/burger.js');
+
+//Get route
+router.get('/burgers', (request, response) => {
+    burger.all((data) => {
+        response.render('index', {
+            burger_data: data
+        });
+    });
+});
+
+//Post route
+router.post('/burgers/create', (request, response) => {
+    burger.create(request.body.burger_name, (result) => {
+        console.log(result);
+        response.redirect('/');
+    });
+});
+
+//Put route
+router.put('/burgers/:id', (request, response) => {
+    burger.update(request.params.id, (result) => {
+        console.log(result);
+        response.sendStatus(200);
+    });
+});
+
+module.exports = router;
